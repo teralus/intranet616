43e941b6f0a676704f1d4d87bee0a9768059cd11
teralus <teraluscamila@outlook.com>
2024-03-11 16:43:04 -0300

    modification of setting file

--- settings/production.py
+++ settings/production.py
@@ -1,1 +1,39 @@
 from .base import *
+
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
+
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'localhost',
+    'intranet.machali.int'
+    ]  # dominio o IP 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
